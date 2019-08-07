ea5ef06a4ecb84254e157565ca6587020002a165
Gwendolyn <gwentowne2002@gmail.com>
2019-08-07 13:21:43 -0400

    Create trial.php

---
+++ phpstuff/trial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+?>
+// Var starts with $, put "" around plaintext, no need to declare various
+// keywords are case insensitive
+// echo is like p, no return value, print is the same with return val 1
+// . to concatenate strings
+// vars are limited to the level they are declared at, unless global using global keywords
+// static carries data from one run of a function to the next
+// echo can take multi parameters, print only one, echo is faster
+// always have ;
+// can use html tags within print-echo
+//
+</body>
+</html>
